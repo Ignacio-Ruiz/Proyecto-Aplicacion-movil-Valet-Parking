aaa76e76ed975021c395d2e17cb25e1bdf967106
AlanGWi <alan.gimilio@alumnos.ulagos.cl>
2022-12-31 16:01:30 -0300

    qr nuevo
    Qr que funciona en emulador

--- fronted/lib/main.dart
+++ fronted/lib/main.dart
@@ -1,93 +1,79 @@
-
+import 'dart:async';
 import 'package:appvalet/models/crono.dart';
 import 'package:flutter/material.dart';
-import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-/// La clase principal de la aplicación Flutter
-void main() {
-  runApp(const MyApp());
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-/// Esta es la clase `MyApp`, que extiende de 'StatelessWidget' y representa la aplicación Flutter
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  String _scanBarcode = 'Unknown';
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-
-        home: Scaffold(
-          appBar: AppBar(title: const Text("valet parking")),
-          body: const Center(
-            child: const HomePage(),
-          ),
-        ));
+  void initState() {
+    super.initState();
   }
 
 
-}
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
 
 
-
-/// Esta es la clase 'HomePage', que extiende de 'StatefulWidget' y representa la página principal de la aplicación
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-/// Esta es la clase '_HomePageState', que extiende de 'State<HomePage>' y representa el estado de la página principal de la aplicación
-class _HomePageState extends State<HomePage> {
-  String result = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            /// Un botón para abrir el escáner de código de barras
-            ElevatedButton(
-              onPressed: () async {
-                /// Navega a la página del escáner de código de barras y obtiene el resultado
-                var res = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SimpleBarcodeScannerPage(),
-                    ));
-                setState(() {
-                  /// Si el resultado es una cadena, asigna el resultado a la variable de resultado
-                  if (res is String) {
-                    result = res;
-                  }
-                });
-              },
-              child: const Text('Open Scanner'),
-            ),
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Barcode scan')),
+            body: Builder(builder: (BuildContext context) {
+              return Container(
+                  alignment: Alignment.center,
+                  child: Flex(
+                      direction: Axis.vertical,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        
+                        ElevatedButton(
+                            onPressed: () => scanQR(),
+                            child: Text('Start QR scan')),
+                        Text('Scan result : $_scanBarcode\n',
+                            style: TextStyle(fontSize: 15)),
 
-            /// Muestra el resultado del escaneo de código de barras
-            Text('Barcode Result: $result'),
-            
-                 ElevatedButton( child: const Text("cronometro"),//el boton , y como se llamara el boton
-
-
-
-             onPressed: () async {//onPressed, quiere decir que cuando se precione se ejecutara lo siguiente
-
+                            ElevatedButton( child: const Text("cronometro"),//el boton , y como se llamara el boton
+             onPressed:  ()  async {//onPressed, quiere decir que cuando se precione se ejecutara lo siguiente
+                
                  var productosData;
                  var hora;
                  var minuto;
                  var time;           
                   http.Response response =
-                  await http.get(Uri.parse(result)); //cambiar por result qr
+                  await http.get(Uri.parse(_scanBarcode)); //cambiar por result qr
               setState(() {
                 productosData = json.decode(response.body);
               
@@ -95,8 +81,8 @@ class _HomePageState extends State<HomePage> {
                 time = productosData['time'];
                 
               });
-
-               if(result !=' '){ //cambiar despues
+               
+               if(_scanBarcode !=' '){ //cambiar despues
               Navigator.push(
                 context,
                 MaterialPageRoute(builder:(context) => Crono(time))
@@ -105,9 +91,7 @@ class _HomePageState extends State<HomePage> {
              }
   
              })
-          ],
-        ),
-      ),
-    );
+                      ]));
+            })));
   }
 }
