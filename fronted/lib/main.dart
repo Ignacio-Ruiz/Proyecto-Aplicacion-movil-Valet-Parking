52c6a3d1343119678b7a46e663840ab193717efd
Ignacio-Ruiz <71133213+Ignacio-Ruiz@users.noreply.github.com>
2022-12-31 21:20:46 -0300

    Cambios

--- fronted/lib/main.dart
+++ fronted/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -22,14 +21,12 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
-
-
   Future<void> scanQR() async {
     String barcodeScanRes;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.QR);
+          '#ff6666', 'Cancelar', true, ScanMode.QR);
       print(barcodeScanRes);
     } on PlatformException {
       barcodeScanRes = 'Failed to get platform version.';
@@ -45,52 +42,67 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(title: const Text('Barcode scan')),
+            appBar: AppBar(
+              title: const Center(
+                  child: Text('Valet Parking', textAlign: TextAlign.center)),
+              backgroundColor: Colors.redAccent,
+            ),
             body: Builder(builder: (BuildContext context) {
               return Container(
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          'images/fondo.jpg'), // establece la imagen de fondo del Container
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                   alignment: Alignment.center,
                   child: Flex(
                       direction: Axis.vertical,
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        
                         ElevatedButton(
                             onPressed: () => scanQR(),
-                            child: Text('Start QR scan')),
-                        Text('Scan result : $_scanBarcode\n',
-                            style: TextStyle(fontSize: 15)),
+                            child: const Text('Escanear')),
+                        Text(
+                          'Resultado Escaner : $_scanBarcode\n',
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white),
+                        ),
+                        ElevatedButton(
+                            child: const Text(
+                                "Verificación"), //el boton , y como se llamara el boton
+                            onPressed: () async {
+                              //onPressed, quiere decir que cuando se precione se ejecutara lo siguiente
 
-                            ElevatedButton( child: const Text("cronometro"),//el boton , y como se llamara el boton
-             onPressed:  ()  async {//onPressed, quiere decir que cuando se precione se ejecutara lo siguiente
-                
-                 var productosData;
-                 var hora;
-                 var minuto;
-                 var time;           
-                  http.Response response =
-                  await http.get(Uri.parse(_scanBarcode)); //cambiar por result qr
-              setState(() {
-                productosData = json.decode(response.body);
-              
-                print(productosData['time']);
-                time = productosData['time'];
-                
-              });
-               
-               if(_scanBarcode !=' '){ //cambiar despues
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder:(context) => Crono(time))
-                //le decimos a que ruta queremos que se vaya.con el nombre de class de widget de la otra pág en este caso 'opcion'
-              );
-             }
-  
-             })
+                              var productosData;
+                              var hora;
+                              var minuto;
+                              var time;
+                              http.Response response = await http.get(Uri.parse(
+                                  _scanBarcode)); //cambiar por result qr
+                              setState(() {
+                                productosData = json.decode(response.body);
+
+                                print(productosData['time']);
+                                time = productosData['time'];
+                              });
+
+                              if (_scanBarcode != ' ') {
+                                //cambiar despues
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Crono(time))
+                                    //le decimos a que ruta queremos que se vaya.con el nombre de class de widget de la otra pág en este caso 'opcion'
+                                    );
+                              }
+                            })
                       ]));
             })));
   }
--- fronted/lib/models/crono.dart
+++ fronted/lib/models/crono.dart
@@ -6,50 +6,31 @@ import '../main.dart';
 import 'package:http/http.dart' as http;
 
 class Crono extends StatefulWidget {
-  
   Crono(this.time);
   String time;
 
-  
-
-
   @override
   State<Crono> createState() => _Crono();
-
 }
 
 class _Crono extends State<Crono> {
-  
-  
-
- var productosData;
-
-   getProductos() async {
-    http.Response response =
-        await http.get(Uri.parse('http://localhost:3000/api/vars/all'));
-    setState(() {
-      productosData = json.decode(response.body) as List;
-      print(productosData);
-    });
-  }
-
+  var productosData;
   Future<Map> getTodoList() async {
-  try {
-    var response = await Dio().get(('http://localhost:3000/api/vars/all'));//resivimos los datos del api para sacar el precio del api
-    print(response.data);
-    print("hola");
-    
-    return response.data;
-      } catch (e) {
-    return <String, Object>{};
+    try {
+      var response = await Dio().get(
+          ('http://10.0.2.2:3000/api/vars/all')); //resivimos los datos del api para sacar el precio del api
+      print(response.data);
+      print("hola");
+
+      return response.data;
+    } catch (e) {
+      return <String, Object>{};
     }
   }
 
-
-
   bool _isStart = true;
   String _stopwatchText = '00:00:00';
-  String _stopwatchText2=" ";
+  String _stopwatchText2 = " ";
   final _stopWatch = new Stopwatch();
   final _timeout = const Duration(seconds: 1);
 
@@ -88,99 +69,98 @@ class _Crono extends State<Crono> {
       _startStopButtonPressed();
     }
     setState(() {
-     _stopWatch.reset();
-     _setStopwatchText(); 
+      _stopWatch.reset();
+      _setStopwatchText();
     });
   }
 
-  Function(){
+  Function() {}
 
-  }
-  
   Future<void> _setStopwatchText() async {
-
-     
-
     var precio;
 
-        http.Response response =
-        await http.get(Uri.parse('http://localhost:3000/api/vars/all'));
+    http.Response response =
+        await http.get(Uri.parse('http://10.0.2.2:3000/api/vars/all'));
     setState(() {
       productosData = json.decode(response.body);
-    
-     // print(productosData[0]['precioM']);
-      precio=productosData[0]['precioM'];
+
+      // print(productosData[0]['precioM']);
+      precio = productosData[0]['precioM'];
       //print(precio);
-      
     });
-     //print(int.parse(precio));
+    //print(int.parse(precio));
     //print(widget.time);
-    var time=widget.time;
+    var time = widget.time;
     var hora;
     var minuto;
     time.toString();
     //print(time.split(":")[0]);
     //print(time.split(":")[1]);
-     hora =time.split(":")[0];
-     minuto = time.split(":")[1];
+    hora = time.split(":")[0];
+    minuto = time.split(":")[1];
 
-      final now = DateTime.now();
-      //print(now.hour);
-     // print(now.minute);
-      var horaactual = now.hour;
-      var minutoactual = now.minute;
+    final now = DateTime.now();
+    //print(now.hour);
+    // print(now.minute);
+    var horaactual = now.hour;
+    var minutoactual = now.minute;
 
     //print(horaactual-int.parse(hora));
-    //print((minutoactual-int.parse(minuto)).abs());  
-    var diferenciaH = (horaactual-int.parse(hora)).abs();
-    var diferenciaM = (minutoactual-int.parse(minuto)).abs();
+    //print((minutoactual-int.parse(minuto)).abs());
+    var diferenciaH = (horaactual - int.parse(hora)).abs();
+    var diferenciaM = (minutoactual - int.parse(minuto)).abs();
 
-    _stopwatchText = (_stopWatch.elapsed.inHours+diferenciaH).toString().padLeft(2,'0') + ':'+
-                     ((_stopWatch.elapsed.inMinutes%60)+diferenciaM).toString().padLeft(2,'0') + ':' +
-                     (_stopWatch.elapsed.inSeconds%60).toString().padLeft(2,'0');
+    _stopwatchText =
+        (_stopWatch.elapsed.inHours + diferenciaH).toString().padLeft(2, '0') +
+            ':' +
+            ((_stopWatch.elapsed.inMinutes % 60) + diferenciaM)
+                .toString()
+                .padLeft(2, '0') +
+            ':' +
+            (_stopWatch.elapsed.inSeconds % 60).toString().padLeft(2, '0');
 
-  
-
- 
-  if (diferenciaH>=1 || diferenciaM>=30 || ((_stopWatch.elapsed.inMinutes%60)+diferenciaM)>=30) {
-    print(diferenciaH);
-    _stopwatchText2= 'precio actual ' +(((_stopWatch.elapsed.inHours+diferenciaH)*60+(_stopWatch.elapsed.inMinutes+diferenciaM)%60)*int.parse(precio)).toString();
-    
-  } 
-  else{
-  _stopwatchText2 = "no hay cobro los primeros 30 minutos";
-    
-  }          
+    if (diferenciaH >= 1 ||
+        diferenciaM >= 30 ||
+        ((_stopWatch.elapsed.inMinutes % 60) + diferenciaM) >= 30) {
+      print(diferenciaH);
+      _stopwatchText2 = 'Precio actual: ' +
+          (((_stopWatch.elapsed.inHours + diferenciaH) * 60 +
+                      (_stopWatch.elapsed.inMinutes + diferenciaM) % 60) *
+                  int.parse(precio))
+              .toString();
+    } else {
+      _stopwatchText2 = "No hay cobro los primeros 30 minutos";
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _buildBody(),
-  
     );
   }
 
   Widget _buildBody() {
     return Column(
       children: <Widget>[
-                Expanded(
+        Expanded(
           child: FittedBox(
-            fit: BoxFit.none,
-            child: Column(children: [Text(
-
-              _stopwatchText,
-              
-              style: TextStyle(fontSize: 60),
-            ),Text(
-              _stopwatchText2,
-               style: TextStyle(fontSize: 60),
-            ),
-            ],)
-          ),
+              fit: BoxFit.none,
+              child: Column(
+                children: [
+                  Text(
+                    _stopwatchText,
+                    style: TextStyle(fontSize: 60),
+                  ),
+                  Text(
+                    _stopwatchText2,
+                    style: TextStyle(fontSize: 60),
+                  ),
+                ],
+              )),
         ),
-        Center(          
-          child: Column(            
+        Center(
+          child: Column(
             children: <Widget>[
               /// Un botón con un icono de "play" o "stop", dependiendo del estado del cronómetro
               ElevatedButton(
@@ -194,9 +174,7 @@ class _Crono extends State<Crono> {
             ],
           ),
         ),
-
       ],
     );
   }
 }
-
